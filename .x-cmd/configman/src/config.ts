2f744c1ee5cfe0b9fbc302a8103347f60276ca8c
L <l@x-cmd.com>
2022-03-04 00:21:34 +0800

    ...

--- .x-cmd/configman/src/config.ts
+++ .x-cmd/configman/src/config.ts
@@ -6,7 +6,7 @@ type Markdown = any
 
 type Version = {
     ver: string,
-    desc: string,
+    desc?: string,
     lic?: string,
     over: {
         add: string,
@@ -32,18 +32,15 @@ type Config = {
             md5: string,
             sha512: string
         }
-    },
-    bin: string[],
-    checkver: {
-        url: string
-    },
-    hook: {
-        pre: {
-            script: string
-        },
-        post: {
-            script: string
-        }
     }
-    
 }
+
+/*
+Where is URL?
+-- Most distribution will be in our repo. So url is fixed.
+-- If this could not be redistributed, we will override the script
+
+Where is checkversion?
+-- Version will be maintained by static-build community.
+-- The actual checkversion facility will be used by maintainer, not user.
+*/
