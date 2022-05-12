e11859f5ff84db86725a78b9ffa6a6e354ee32b1
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-12 15:40:43 +0000

    feat(pkg): add node version.sh

---
+++ src/lang/node/version.sh
@@ -0,0 +1,65 @@
+# shellcheck shell=sh disable=SC2039,SC2142,SC3043 #source
+
+# curl https://nodejs.org/dist/index.json | jq -r '.[] | {
+#     (.version) : {
+#         security: (.security)
+#     }
+# }' | jq -s add
+
+if ! [ -f tmp/a ] ; then
+    mkdir tmp
+    curl https://nodejs.org/dist/index.json > "tmp/a"
+fi
+
+aaa(){
+    cat tmp/a | ___x_cmd_jo env .\* .version .files .security  -- 'echo "$version
+$security
+$files"'
+}
+
+aaa | awk '
+BEGIN{
+    DATA_VERSION = 0
+    DATA_SECURITY = 1
+    DATA_FILES = 2
+    DATA_STATUS = DATA_VERSION
+}
+DATA_STATUS==DATA_VERSION{
+    version =  $0 ":\n"
+    DATA_STATUS = DATA_SECURITY
+    next
+}
+DATA_STATUS==DATA_SECURITY{
+    security = $0 "\n"
+    DATA_STATUS = DATA_FILES
+    next
+}
+DATA_STATUS==DATA_FILES{
+    DATA_STATUS = DATA_VERSION
+    if ($0 == "[src]") {
+        version=""
+        security=""
+        next
+    }
+    line=substr($0,2 ,length($0)-2 )
+    gsub(/src,|-tar|-pkg|-7z|-exe|-msi|-zip|headers,/, "",line)
+    line = unique(line)
+    data = data version "  security: "security "  " line ":\n"
+    next
+}
+END{
+    print data
+}
+
+function unique(line,       lastvalue, arr, l, _, i){
+    l = split(line, arr, ",")
+    for(i = 1; i <= l; i++){
+        if(arr[i] != lastvalue){
+            _ = (_ != "") ? _ ":\n  " arr[i] : arr[i]
+        }
+        lastvalue = arr[i]
+    }
+    return _
+}
+
+'
