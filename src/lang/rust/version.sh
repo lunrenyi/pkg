87f75ed156bb2d9b60407478847f4bbd212e56e2
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-13 06:56:16 +0000

    feat(pkg): add rust version.sh

---
+++ src/lang/rust/version.sh
@@ -0,0 +1,20 @@
+# shellcheck shell=sh  #source
+
+# v1.45.2: {}
+# v1.56.1: {}
+# v1.60.0: {}
+
+get_rust_version(){
+    curl https://forge.rust-lang.org/infra/other-installation-methods.html 2>/dev/null | awk 'BEGIN{
+        print "rust-1.60.0:"
+    }
+    {
+        if ($0 ~ "---------|--------|------|--------") INFO_STATUS = 1
+        else if ($0 ~ "`, and") exit(0)
+        else if (INFO_STATUS == 1) {
+            if (match($0, "` | ")) print "  " substr($0, 2, RSTART-2) ":\n    sha:"
+        }
+        else next
+    }'
+}
+get_rust_version | x yq -o json e -P
