6bd65a2d16ae99f1c887e9a386ad66b9e4159a11
litinghui <litinghui@x-cmd.com>
2022-05-13 21:12:12 +0800

    update java/post-install.sh

--- src/lang/java/post-install.sh
+++ src/lang/java/post-install.sh
@@ -2,7 +2,7 @@ ___x_cmd_env_java_copy_to_unpackdir(){
 	local version="$1"
 
 	case "$version" in
-		*-open)
+		*-open|*-sapmchn|*-trava|*-oracle|*-grl|*-gln|*-amzn|*-nik|*-tem)
 			if [ "$(x os name)" = "darwin" ]; then
 				env:debug "Copying " "$unpack_dir"/*/Contents/Home/* " to " "$unpack_dir"/*/
 				mv "$unpack_dir"/*/Contents/Home/* "$unpack_dir"/*/
