54d97cb2eccd25801b0271e4285e4b3e8fbd0441
litinghui <litinghui@x-cmd.com>
2022-05-13 21:06:53 +0800

    update get_sdk_info

--- src/lang/java/post-install.sh
+++ src/lang/java/post-install.sh
@@ -1,9 +1,15 @@
 ___x_cmd_env_java_copy_to_unpackdir(){
-	if [ "$(x os name)" = "darwin" ]; then
-		env:debug "Copying " "$unpack_dir"/*/Contents/Home/* " to " "$unpack_dir"/*/
-		mv "$unpack_dir"/*/Contents/Home/* "$unpack_dir"/*/
-		x rmrf "$unpack_dir"/*/Contents
-	fi
+	local version="$1"
+
+	case "$version" in
+		*-open)
+			if [ "$(x os name)" = "darwin" ]; then
+				env:debug "Copying " "$unpack_dir"/*/Contents/Home/* " to " "$unpack_dir"/*/
+				mv "$unpack_dir"/*/Contents/Home/* "$unpack_dir"/*/
+				x rmrf "$unpack_dir"/*/Contents
+			fi
+			;;
+	esac
 }
 
-___x_cmd_env_java_copy_to_unpackdir
+___x_cmd_env_java_copy_to_unpackdir "$@"
