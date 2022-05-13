0ba142e5f9954e2059c16a1ffe90e38fa710476c
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-13 07:01:35 +0000

    feat(pkg): update python and scala version.sh

--- src/lang/python/version.sh
+++ src/lang/python/version.sh
@@ -1,21 +1,12 @@
-
-if ! [ -f tmp/py_miniconda ] ; then
-  mkdir tmp
-  curl https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/ > "tmp/py_miniconda"
-fi
-
-if ! [ -f tmp/py_miniconda ] ; then
-  mkdir tmp
-  curl -H "Accept-Encoding: gzip" https://downloads.python.org/pypy/ | gunzip | more > "tmp/py_pypy"
-fi
+# shellcheck shell=sh #source
 
 get_py_version(){
-cat tmp/py_miniconda | awk 'match($0,"Miniconda" "-" "[0-9.]+" "-" "[A-Za-z]+" "-" "[A-Za-z0-9_]+") { a = substr($0,RSTART,RLENGTH)
+curl https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/ 2>/dev/null | awk 'match($0,"Miniconda" "-" "[0-9.]+" "-" "[A-Za-z]+" "-" "[A-Za-z0-9_]+") { a = substr($0,RSTART,RLENGTH)
   split(a,b,"-")
   print b[2] "-" b[1] ":\n  " b[3] "-" b[4] ":\n  sha:"
 }'
 
-cat tmp/py_pypy | awk ' match($0, "pypy[0-9.]+" "-" "v[0-9.]+" "-" "[a-z0-9]+" ){
+curl -H "Accept-Encoding: gzip" https://downloads.python.org/pypy/ 2>/dev/null | gunzip | more | awk ' match($0, "pypy[0-9.]+" "-" "v[0-9.]+" "-" "[a-z0-9]+" ){
   a = substr($0,RSTART,RLENGTH)
   split(a,b,"-")
   if(b[3] != "src"){print b[2] "-" b[1] ":\n  " b[3] ":\n  sha:" }
