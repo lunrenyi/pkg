05db6fbd1ebc8163c9e569f040fbf3ac65e3739d
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-22 02:19:00 +0000

    update

--- src/lang/deno/.x-cmd/install.sh
+++ src/lang/deno/.x-cmd/install.sh
@@ -2,7 +2,7 @@ ___x_cmd_deno_unpack(){
     local file_suffix
     ___x_cmd_pkg___attr "$pkg_name" "$version" "$osarch" file_suffix
     local ball="$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/$version.$file_suffix"
-    local tgt="$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version"
+    local tgt="$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version/bin"
 
     local bin_path
     bin_path="$(___x_cmd_pkg_xbin_path "$pkg_name" "$version")"
