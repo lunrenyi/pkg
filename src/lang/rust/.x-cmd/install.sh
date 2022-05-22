7d4f783cbceeb1934ebe5e2541750b83e946fc22
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-22 07:58:12 +0000

    update

--- src/lang/rust/.x-cmd/install.sh
+++ src/lang/rust/.x-cmd/install.sh
@@ -1,18 +1,6 @@
-___x_cmd_pkg_rust_unpack()
-    # local file_suffix
-    # ___x_cmd_pkg___attr "$pkg_name" "$version" "$osarch" file_suffix
-
-    # local ball="$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/$version.$file_suffix"
-    local tgt="$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version"
-    local unpack_dir="$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/rust-$version"
-    # x rmrf "$tgt" && mkdir -p "$tgt"
-    # pkg:debug "Moving $unpack_dir to $tgt"
-    # local bin_path
-    # bin_path="$(___x_cmd_pkg_xbin_path "$pkg_name" "$version")"
-    # if [ -x "$bin_path" ]; then
-    #     pkg:debug "rust $version had been unpacked."
-    #     return 0
-    # fi
+___x_cmd_pkg_rust_unpack()(
+    local tgt="$___X_CMD_PKG_INSTALL_PATH/$pkg_name/rust-$version"
+    local unpack_dir="$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/$version/tmp"
 
     x uz "$ball" "$unpack_dir" 1>/dev/null || {
         pkg:warn "Fail to unzip $ball"
@@ -25,9 +13,7 @@ ___x_cmd_pkg_rust_unpack()
     fi
     ./install.sh --destdir="../$version/" --prefix=/
     cd .. && x rmrf "$tgt"
-    # mv -f "$unpack_dir"/*/* "$tgt"
-    x rmrf "$unpack_dir"
-    # echo $PWD
+    x rmrf "$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/$version"
 
 )
 
