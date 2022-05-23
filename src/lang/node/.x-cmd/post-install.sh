eb946c7a670bdec414480b0a437db8933c3bf8a5
zhengyuhang <zhengyuhang@x-cmd.com>
2022-05-23 20:30:50 +0800

    fix

--- src/lang/node/.x-cmd/post-install.sh
+++ src/lang/node/.x-cmd/post-install.sh
@@ -1,10 +1,18 @@
-___x_cmd_env_node_copy_to_unpackdir(){
+___x_cmd_pkg_node_unpack(){
     if [ $(x os name) = "win" ] ; then
     (
-        cd "${unpack_dir}"/*/ && chmod +x node.exe npm npx
-        mkdir -p "${unpack_dir}/tmp/bin"
-        mv -f "$PWD"/* "${unpack_dir}/tmp/bin"
-        x rmrf "$PWD"
+        local ball="$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/$version.$file_suffix"
+        local unpack_dir="$___X_CMD_PKG_DOWNLOAD_PATH/$pkg_name/$version"
+        local tgt="$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version"
+
+        x uz "$ball" "tgt" || {
+        pkg:warn "Fail to unzip $ball"
+    }
+
+        cd "${tgt}"/*/ && chmod +x node.exe npm npx
+        # mkdir -p "${unpack_dir}/tmp/bin"
+        # mv -f "$PWD"/* "${unpack_dir}/tmp/bin"
+        # x rmrf "$PWD"
     )
     else
         ___x_cmd_pkg_install___unzip "$pkg_name" "$version" "$osarch"
