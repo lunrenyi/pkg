f64adb1c8c1fec58c1d5fb68c53f4901156cbfcf
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-24 12:20:05 +0000

    update all.7z

--- src/lang/deno/.x-cmd/install.sh
+++ src/lang/deno/.x-cmd/install.sh
@@ -2,7 +2,7 @@ ___x_cmd_deno_unpack(){
     local tgt="$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version/bin"
 
     pkg:info "Unpack: $ball to $tgt"
-    unzip -d "$tgt" "$ball"  || {
+    x uz "$ball" "$tgt"  || {
         pkg:warn "Fail to unzip $ball"
     }
 
