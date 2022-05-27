a7afb34a575cfa71c5a67c5e5065f5554c692cc3
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-27 01:27:02 +0000

    update

--- src/media/ffmpeg/.x-cmd/install.sh
+++ src/media/ffmpeg/.x-cmd/install.sh
@@ -1,5 +1,5 @@
 ___x_cmd_ffmpeg_unpack()(
-    ___x_cmd_ensuredir "$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version"
+    x mkdirp "$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version"
     cd "$___X_CMD_PKG_INSTALL_PATH/$pkg_name/$version" || exit
     pkg:info "$ball"
     p7zd x "$ball" 1>/dev/null 2>/dev/null
