222ebbd4af6c534b61f2c4b1961410c3d5a8e57a
zhengyuhang <zhengyuhang@x-cmd.com>
2022-05-28 14:52:30 +0800

    ...

--- src/lang/go/.x-cmd/activate.sh
+++ src/lang/go/.x-cmd/activate.sh
@@ -1,6 +1,15 @@
 # shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_go_activate(){
     x path add_existed_folder "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
+
+    # if [ "${}" != 'system' ] ; then
+	#     if [ "unset" = "${GOROOT:-unset}" ] ; then
+	#         export GOROOT=$___X_CMD_PKG_INSTALL_PATH/go
+	#     fi
+	#     if [ "unset" = "${GOPATH:-unset}" ] ; then
+	#         export GOPATH=$___X_CMD_PKG_INSTALL_PATH/packages
+	#     fi
+    # fi
 }
 
 ___x_cmd_go_activate
--- src/lang/perl/version.sh
+++ src/lang/perl/version.sh
@@ -7,7 +7,7 @@ get_perl_version(){
     lastosarch = ""
     version = substr($0,RSTART+9,RLENGTH-10)
   }
-  else if(match($0, /"name": "perl-[a-z0-9]*-[a-z0-9]*/)){   #"
+  else if(match($0, /"name": "perl-[a-z0-9_]*-[a-z0-9]*/)){   #"
     tmp = substr($0,RSTART+14,RLENGTH-14)
     split (tmp,a,"-")
     osarch = a[2]"/"a[1]
@@ -23,20 +23,13 @@ function print_osarch(osarch){
   curl https://strawberryperl.com/releases.html 2>/dev/null |awk '
     match($0, /<td><b>[0-9.]+<\/b><\/td>/) {
     version = substr($0,RSTART+7,RLENGTH-16)
-    print version " 32bit"
-    print version " 64bit"
-}
+    print version":\n  x64:\n    sha:"
+    }
 '
 }
 
 get_perl_info_to_yml(){
-  get_perl_version  | sort -V -u -r | awk '{
-    if(last != $1){
-      print $1 ":"
-    }
-    print "  " $2 ":\n    sha:"
-    last = $1
-  }'
+  get_perl_version  | sort -V -u -r
 }
 
 get_perl_version | x yq -o json e -P > "/home/linux/work/x-cmd/pkg/src/lang/perl/tmp"
