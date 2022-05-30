9016493459af3d0efc8350c5f3d6dd501bd79cd3
zhengqiakai <zhengqiakai@x-cmd.com>
2022-05-30 12:38:23 +0000

    update

--- src/lang/lua/.x-cmd/install.sh
+++ src/lang/lua/.x-cmd/install.sh
@@ -3,8 +3,9 @@ ___x_cmd_pkg_lua_unpack(){
     local tgt="$___X_CMD_PKG_INSTALL_PATH/$name/$version"
     pkg:info "Unpack: $ball to $tgt"
     x rmrf "$tgt"; mkdir -p "$tgt"
-    x uz "$ball" "$tgt" || {
+    x uz "$ball" "$tgt" 2>/dev/null || {
         pkg:warn "Fail to unzip $ball"
     }
+    pkg:info "Finish unpack"
 }
 ___x_cmd_pkg_lua_unpack
