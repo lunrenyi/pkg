6259a0ee255d6405980fcb865d3e321b9ba5cd89
zhengqiakai <zhengqiakai@x-cmd.com>
2022-06-06 04:13:44 +0000

    update julia

--- src/lang/julia/version.sh
+++ src/lang/julia/version.sh
@@ -3,7 +3,7 @@
 
 get_julia_version_cn(){
     curl https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/versions.json 2>/dev/null | x jo env .\*.files.\* .version .os .arch .url .sha256 -- 'echo "$version $os/$arch" $url $sha256' \
-    | sort -V -u -r | awk '{
+    | sort -V -u -r | awk '$3 !~ /(exe)$|(dmg)$/{
         if ($1 != last_version) {
             last_version = $1
             gsub("winnt", "win", $2)
