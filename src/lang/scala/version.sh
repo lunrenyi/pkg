239512256e8df4b0a3b8be4e40023e8532ec11d1
Zhengyuhang <mnnna@x-cmd.com>
2022-06-10 20:18:30 +0800

    ...

--- src/lang/scala/version.sh
+++ src/lang/scala/version.sh
@@ -9,18 +9,6 @@ get_scala_version(){
         print a":\n  " "darwin/amd64:\n    sha:"
         print a":\n  " "win/x32:\n    sha:"
     } '
-
-    # curl "http://distfiles.macports.org/scala3/"  2>/dev/null | awk 'match($0,"scala3-3.0.2"){a = substr($0,RSTART+7,RLENGTH-7)
-    #     print a"(cn)"":\n  " "linux/x64:\n    sha:"
-    #     print a"(cn)"":\n  " "linux/x32:\n    sha:"
-    #     print a"(cn)"":\n  " "darwin/x64:\n    sha:"
-    #     print a"(cn)"":\n  " "win/64:\n    sha:"
-    #     print a"(cn)"":\n  " "darwin/amd64:\n    sha:"
-    #     print a"(cn)"":\n  " "win/x32:\n    sha:"
-    # }'
-
-
-
 }
 
 get_scala_version | x yq -o json e -P
