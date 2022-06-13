7aefcaf990e4e913224fac0ce663c76fc71a4e74
Zhengqbbb <1074059947@qq.com>
2022-06-13 16:02:30 +0800

    feat(zsh-plugin): done zsh-plugin activate and deactivate
    current using zsh syntax to parse array.wait pkg bug fixed

--- src/plugin/zsh-plugin/.x-cmd/activate.sh
+++ src/plugin/zsh-plugin/.x-cmd/activate.sh
@@ -1,5 +1,21 @@
-# shellcheck shell=sh disable=
+# shellcheck shell=sh disable=SC2154,SC1090,SC3043
+
 ___x_cmd_zsh_plugin_activate(){
-    echo 1111
+    [ -n "$ZSH_VERSION" ] || return 1
+    local zsh_plugins
+    local plugin
+    zsh_plugins=("${(@s/,/)ZSH_PLUGIN:?Provide ZSH plugin name}")
+    for plugin in "${zsh_plugins[@]}"; do
+        if . "$___X_CMD_PKG_INSTALL_PATH/$name/$version/$plugin/$plugin.plugin.zsh"; then
+            x boot rc add "x-plugin-$plugin" \
+                "[ -n \"\$ZSH_VERSION\" ] && . $___X_CMD_PKG_INSTALL_PATH/$name/$version/$plugin/$plugin.plugin.zsh"
+        fi
+    done
+
+    # TODO: wait pkg activate bug fix
+    # if . "$___X_CMD_PKG_INSTALL_PATH/$name/$version/${ZSH_PLUGIN:?Provide ZSH plugin name}/$ZSH_PLUGIN.plugin.zsh"; then
+    #     x boot rc add "x-plugin-$ZSH_PLUGIN" "[ -n \"\$ZSH_VERSION\" ] && . $___X_CMD_PKG_INSTALL_PATH/$name/$version/$ZSH_PLUGIN/$ZSH_PLUGIN.plugin.zsh"
+    # fi
 }
+
 ___x_cmd_zsh_plugin_activate
--- src/plugin/zsh-plugin/.x-cmd/deactivate.sh
+++ src/plugin/zsh-plugin/.x-cmd/deactivate.sh
@@ -1,1 +1,15 @@
-# shellcheck shell=sh disable=
+# shellcheck shell=sh disable=SC2154,SC1090,SC3043
+
+___x_cmd_zsh_plugin_deactivate(){
+    [ -n "$ZSH_VERSION" ] || return 1
+    local zsh_plugins
+    local plugin
+    zsh_plugins=("${(@s/,/)ZSH_PLUGIN:?Provide ZSH plugin name}")
+    for plugin in "${zsh_plugins[@]}"; do
+        x boot rc del "x-plugin-${plugin}"
+    done
+    # TODO: wait pkg activate bug fix
+    # x boot rc del "x-plugin-${ZSH_PLUGIN:?Provide ZSH plugin name}"
+}
+
+___x_cmd_zsh_plugin_deactivate
