10c1054260d9866bc4ebc0dbb8cfbdeda0e446e9
mnnna <850400628@qq.com>
2022-06-13 11:18:13 +0800

    fix node

--- src/lang/node/.x-cmd/activate.sh
+++ src/lang/node/.x-cmd/activate.sh
@@ -2,8 +2,8 @@ ___x_cmd_node_activate(){
     x path add_existed_folder "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
 
     if [ "${NPM_CONFIG_PREFIX-}" = "" ] && [ -n "$___X_CMD_PKG_INSTALL_PATH" ]; then
-        export NPM_CONFIG_PREFIX="$___X_CMD_PKG_INSTALL_PATH/$version/.npm"
-        x path add "$___X_CMD_PKG_INSTALL_PATH/$version/.npm/bin"
+        export NPM_CONFIG_PREFIX="$___X_CMD_PKG_INSTALL_PATH/$name/$version/.npm"
+        x path unshift "$___X_CMD_PKG_INSTALL_PATH/$name/$version/.npm/bin"
     fi
 }
 
