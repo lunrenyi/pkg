99e1becf462cdd2cc9c63ca4351548a3426ac2ee
mnnna <850400628@qq.com>
2022-06-16 09:45:44 +0800

    ...

--- src/lang/go/.x-cmd/activate.sh
+++ src/lang/go/.x-cmd/activate.sh
@@ -1,4 +1,4 @@
-shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
+#shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_go_activate(){
     mkdir -p "$___X_CMD_PKG_INSTALL_PATH/Go_package"
     x path add_existed_folder "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
