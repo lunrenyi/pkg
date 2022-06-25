c377e3de2443aa61e50549a31fb0b585b2187086
tzw <tangzhiwen@x-cmd.com>
2022-06-25 01:22:35 +0000

    !5 fix zellij activate.sh * update zellij

--- src/osman/zellij/.x-cmd/activate.sh
+++ src/osman/zellij/.x-cmd/activate.sh
@@ -1,5 +1,5 @@
 # shellcheck shell=sh disable=SC2154,SC1090,SC3043
-___x_cmd_zsh_plugin_activate(){
-    export SHELL=$0
+___x_cmd_zellij_premise_activate(){
+    export SHELL="$___X_CMD_CUR_SHELL"
 }
-___x_cmd_zsh_plugin_activate
+___x_cmd_zellij_premise_activate
