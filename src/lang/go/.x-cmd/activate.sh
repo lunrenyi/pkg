a1acb2ddc2188ed4e85d666f1e04304a0c0f265e
litinghui <litinghui@x-cmd.com>
2022-07-01 11:03:19 +0800

    fix: Double quote to prevent globbing and word splitting.shellcheck(SC2086)

--- src/lang/go/.x-cmd/activate.sh
+++ src/lang/go/.x-cmd/activate.sh
@@ -1,6 +1,6 @@
 #shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_go_activate(){
-    [ -d $___X_CMD_PKG_INSTALL_PATH/Go_package ] || mkdir -p "$___X_CMD_PKG_INSTALL_PATH/Go_package"
+    [ -d "$___X_CMD_PKG_INSTALL_PATH"/Go_package ] || mkdir -p "$___X_CMD_PKG_INSTALL_PATH/Go_package"
     ___x_cmd_path_unshift "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
 	export GOPATH="$___X_CMD_PKG_INSTALL_PATH/$name/$version/Go_package"
 }
