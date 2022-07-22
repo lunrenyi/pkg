be6a11ab40d9d4a4eecb36d24a45c5543c14a07e
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-22 14:50:58 +0800

    fix

--- src/lang/java/version.sh
+++ src/lang/java/version.sh
@@ -1,23 +1,5 @@
 # shellcheck shell=sh  #source
 get_java_version(){
-for platform in mingw64_nt_10 linuxarm64 linuxx64 darwinx64 darwinarm64 ; do
-
-    curl "https://api.sdkman.io/2/candidates/java/${platform}/versions/all" 2>/dev/null | awk -v platform=$platform '
-          BEGIN{ RS=","} {
-            gsub("mingw64_nt_10","win/x64",platform)
-            gsub("linuxarm64","linux/arm64",platform)
-            gsub("linuxx64","linux/x64",platform)
-            gsub("darwinx64","darwin/x64",platform)
-            gsub("darwinarm64","darwin/arm64",platform)
-            print $0 "  "  platform }
-    '
-done  | sort -V -u -r | awk '{
-        if(last != $1){
-            print $1 ":"
-        }
-        print "  " $2 ":\n    sha:"
-        last = $1
-    }'
 wget --no-check-certificate  "https://www.injdk.cn/" 2>/dev/null
 cat "/home/mnnna/work/x-cmd/pkg/index.html" 2>/dev/null | awk '
     match($0,"oraclejdk" "/" "[0-9]*" "/" "jdk-[0-9._a-z]+" "[-]?" "[0-9a-zA-Z_]+" "." "[0-9a-z.]+"){a = substr($0,RSTART,RLENGTH)
@@ -38,5 +20,24 @@ cat "/home/mnnna/work/x-cmd/pkg/index.html" 2>/dev/null | awk '
     }
 '
     x rmrf index.html
+
+for platform in mingw64_nt_10 linuxarm64 linuxx64 darwinx64 darwinarm64 ; do
+
+    curl "https://api.sdkman.io/2/candidates/java/${platform}/versions/all" 2>/dev/null | awk -v platform=$platform '
+          BEGIN{ RS=","} {
+            gsub("mingw64_nt_10","win/x64",platform)
+            gsub("linuxarm64","linux/arm64",platform)
+            gsub("linuxx64","linux/x64",platform)
+            gsub("darwinx64","darwin/x64",platform)
+            gsub("darwinarm64","darwin/arm64",platform)
+            print $0 "  "  platform }
+    '
+done  | sort -V -u -r | awk '{
+        if(last != $1){
+            print $1 ":"
+        }
+        print "  " $2 ":\n    sha:"
+        last = $1
+    }'
 }
 get_java_version | x yq -o json e -P
