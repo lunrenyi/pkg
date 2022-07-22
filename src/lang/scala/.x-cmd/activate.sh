2045c04782a0a935f622cc8ac6fd42bd9c34145a
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-22 15:35:21 +0800

    ..

--- src/lang/scala/.x-cmd/activate.sh
+++ src/lang/scala/.x-cmd/activate.sh
@@ -1,5 +1,6 @@
 # shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_scala_activate(){
+    chmod +x "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
     ___x_cmd_path_unshift "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
 }
 
