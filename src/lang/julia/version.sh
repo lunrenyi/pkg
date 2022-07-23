3ae5839b9121fab7ec0bf96fb8fea561123373eb
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-23 11:57:10 +0800

    fix

--- src/lang/julia/version.sh
+++ src/lang/julia/version.sh
@@ -1,10 +1,12 @@
 # shellcheck shell=sh  #source
 
 
+# | sort -V -u -r | awk '$4 !~ /(dmg)$/{
 get_julia_version_cn(){
     curl https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/versions.json 2>/dev/null | x jo env .\*.files.\* .version .os .arch .url .sha256 -- 'echo "$version $os $arch $url $sha256"' \
-    | sort -V -u -r | awk '$4 !~ /(dmg)$/{
+    | sort -V -u -r | awk '{
         file_suffix = ($2 != "winnt") ? "tar.gz" : "exe"
+        file_suffix = ($2 == "mac") ? "tar.gz" : file_suffix
         version = substr( $1, 1, 3)
         osarch = $2 "/" $3
         gsub("winnt", "win", osarch)
@@ -13,12 +15,15 @@ get_julia_version_cn(){
         gsub("x86_64", "x64", $3)
         gsub("i686", "x86", $3)
 
+
         _osarch = $2 "-" $3
         if ( _osarch == "winnt-x64" ) _osarch = "win64"
         else if ( _osarch == "winnt-x86"  ) _osarch = "win32"
         else if ( _osarch == "linux-x64"  ) _osarch = "linux-x86_64"
         else if ( _osarch == "linux-x86"  ) _osarch = "linux-i686"
         else if ( _osarch == "linux-arm64"  ) _osarch = "linux-arm"
+        else if ( _osarch == "mac-x64"  )   _osarch = "mac64"
+        else if ( _osarch == "arm-x64"  )   _osarch = "aarch64"
 
         if ($1 != last_version) {
             last_version = $1
