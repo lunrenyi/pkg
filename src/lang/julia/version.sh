474e6718d8c03feaf62704e92e235ee45ca7d233
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-23 15:57:00 +0800

    fix

--- src/lang/julia/version.sh
+++ src/lang/julia/version.sh
@@ -6,6 +6,7 @@ get_julia_version_cn(){
     curl https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/versions.json 2>/dev/null | x jo env .\*.files.\* .version .os .arch .url .sha256 -- 'echo "$version $os $arch $url $sha256"' \
     | sort -V -u -r | awk '{
         file_suffix = ($2 != "winnt") ? "tar.gz" : "exe"
+        gsub("exe","tar.gz",file_suffix)
         file_suffix = ($2 == "mac") ? "tar.gz" : file_suffix
         version = substr( $1, 1, 3)
         osarch = $2 "/" $3
@@ -28,9 +29,9 @@ get_julia_version_cn(){
 
         if ($1 != last_version) {
             last_version = $1
-            print $1 ":\n  " osarch ":\n    url:\n      _: " $4 "\n      cn: " "https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/" $2 "/" $3  "/" version "/julia-" $1 "-" _osarch "." file_suffix "\n    sha256: " 
+            print $1 ":\n  " osarch ":\n    url:\n      _: " $4 "\n      cn: " "https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/" $2 "/" $3  "/" version "/julia-" $1 "-" _osarch "." file_suffix
         } else {
-            print "  " osarch ":\n    url:\n      _: " $4 "\n      cn: " "https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/" $2 "/" $3 "/" version "/julia-" $1 "-" _osarch "." file_suffix "\n    sha256: "
+            print "  " osarch ":\n    url:\n      _: " $4 "\n      cn: " "https://mirrors.tuna.tsinghua.edu.cn/julia-releases/bin/" $2 "/" $3 "/" version "/julia-" $1 "-" _osarch "." file_suffix
         }
     }
     '
