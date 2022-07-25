0887c5b113833a129cd6017402e6d81613dfacf5
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-25 10:34:54 +0800

    ...

--- src/lang/java/.x-cmd/activate.sh
+++ src/lang/java/.x-cmd/activate.sh
@@ -1,10 +1,15 @@
 # shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_java_activate(){
+    local arch
+    arch="$(___x_cmd_os arch)"
+    local osname
+    osname="$(___x_cmd_os name)"
     ___x_cmd_path_unshift "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
-
-    if [ -n "$___X_CMD_PKG_INSTALL_PATH" ]; then
+    if [ $arch = "x64" ] || [ $osname = "darwin" ];then
+        export JAVA_HOME="$___X_CMD_PKG_INSTALL_PATH/$name/$version/Contentss/Home/bin"
+    elif [ -n "$___X_CMD_PKG_INSTALL_PATH" ]; then
         export JAVA_HOME="$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
-        export PATH=$JAVA_HOME/bin:$PATH
+
     fi
 }
 
