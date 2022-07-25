4a8ce836aeede82a993e1e8ca9df0e770f83eccf
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-25 10:12:27 +0800

    ...

--- src/lang/java/.x-cmd/activate.sh
+++ src/lang/java/.x-cmd/activate.sh
@@ -4,6 +4,7 @@ ___x_cmd_java_activate(){
 
     if [ -n "$___X_CMD_PKG_INSTALL_PATH" ]; then
         export JAVA_HOME="$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
+        export PATH=$JAVA_HOME/bin:$PATH
     fi
 }
 
