0a0c7f20250b2cc20b41ea38b704233bc9bc3259
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-25 10:47:14 +0800

    ...

--- src/lang/java/.x-cmd/deactivate.sh
+++ src/lang/java/.x-cmd/deactivate.sh
@@ -1,6 +1,14 @@
 # shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_java_deactivate(){
-    x path rm "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
+    local arch
+    arch="$(___x_cmd_os arch)"
+    local osname
+    osname="$(___x_cmd_os name)"
+    if [ $arch = "x64" ] && [ $osname = "darwin" ];then
+        x path rm "$___X_CMD_PKG_INSTALL_PATH/$name/$version/Contents/Home/bin"
+    elif [ -n "$___X_CMD_PKG_INSTALL_PATH" ]; then
+        x path rm "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
+    fi
 }
 
 ___x_cmd_java_deactivate
