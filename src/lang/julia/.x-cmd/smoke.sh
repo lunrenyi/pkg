edf65bec4a67c3a6ec433440d77621d3b8f2dd29
zhengyuhang <zhengyuhang@x-cmd.com>
2022-07-29 16:12:49 +0800

    ...

--- src/lang/julia/.x-cmd/smoke.sh
+++ src/lang/julia/.x-cmd/smoke.sh
@@ -1,7 +1,6 @@
 pkg:info "julia"
 julia_test(){
-    echo $___X_CMD_PKG_INSTALL_PATH/$name/$version/bin/julia
-x assert stdout 'eval $___X_CMD_PKG_INSTALL_PATH/$name/$version/bin/julia -v' <<A
+x assert stdout '$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin/julia -v' <<A
 julia version 1.8.0-rc1
 A
 }
