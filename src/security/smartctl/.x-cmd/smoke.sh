c10783961c5559e0f376a1e02a1fa5a22bfca376
tzw <tangzhiwen@x-cmd.com>
2023-01-24 17:11:20 +0800

    add  smartctl

---
+++ src/security/smartctl/.x-cmd/smoke.sh
@@ -0,0 +1,13 @@
+pkg:info "bandwhich"
+bandwhich_test(){
+    local arch
+    arch="$(___x_cmd_os arch)"
+    local osname
+    osname="$(___x_cmd_os name)"
+
+    x assert stdout eval '$___X_CMD_PKG_INSTALL_PATH/$name/$version/bandwhich.${osname}.${arch} -V' <<A
+bandwhich 0.20.0
+A
+}
+
+bandwhich_test
