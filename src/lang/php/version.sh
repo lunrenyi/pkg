70cc18706549c49f21d9125dc3c8b44ac201403c
zhengyuhang <zhengyuhang@x-cmd.com>
2023-02-21 13:49:39 +0800

    ...

--- src/lang/php/version.sh
+++ src/lang/php/version.sh
@@ -2,7 +2,12 @@
 get_php_version(){
     curl http://php.p2hp.com/downloads.php 2>/dev/null | awk '{
       if( match($0, "^[ ]*PHP [0-9.]+")){
-        print substr($0,RSTART+8,RLENGTH-8) ":\n" "     sha256:"
+        print substr($0,RSTART+8,RLENGTH-8) ":\n"
+        print "  linux/x64:\n    sha: null"
+        print "  darwin/amd64:\n    sha: null"
+        print "  darwin/x86:\n    sha: null"
+        print "  win/x64:\n    sha: null"
+        print "  darwin/arm64:\n    sha: null"
       }
 
     }'
