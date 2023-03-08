4384a1f17245ebdef33c5a8fd919d412be8dbffd
litinghui <litinghui@x-cmd.com>
2023-03-08 16:00:22 +0800

    fix update go path

--- src/lang/go/.x-cmd/activate.sh
+++ src/lang/go/.x-cmd/activate.sh
@@ -2,6 +2,7 @@
 ___x_cmd_go_activate(){
     [ -d "$___X_CMD_PKG_INSTALL_PATH"/$name/$version/Go_package ] || mkdir -p "$___X_CMD_PKG_INSTALL_PATH/$name/$version/Go_package"
     ___x_cmd_path_unshift "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
-	export GOPATH="$___X_CMD_PKG_INSTALL_PATH/$name/$version/Go_package"
+    export GOPATH="$___X_CMD_PKG_INSTALL_PATH/$name/$version/Go_package"
+    ___x_cmd_path_unshift "$GOPATH/bin"
 }
 ___x_cmd_go_activate
