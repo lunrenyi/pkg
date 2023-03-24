d9dfe6e89ab4bc4c3f56eeb2d7fdb61e2ae1a5e3
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-24 17:20:56 +0800

    ...

--- src/media/agg/.x-cmd/activate.sh
+++ src/media/agg/.x-cmd/activate.sh
@@ -1,5 +1,4 @@
 ___x_cmd_agg_activate(){
-echo "ff"
     if ! [ -f "$___X_CMD_PKG_BINARY_PATH/$name" ]; then
         command ln -s "$___X_CMD_PKG_INSTALL_PATH/$name/$version/$name.$os.$arch"  "$___X_CMD_PKG_BINARY_PATH"
     fi
