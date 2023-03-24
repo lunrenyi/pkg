e7ab1a4e0bdcd400fa989059a06316d67ca98a6e
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-24 14:59:57 +0800

    ...

---
+++ src/media/agg/.x-cmd/activate.sh
@@ -0,0 +1,4 @@
+___x_cmd_agg_activate(){
+    ___x_cmd_path_unshift "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
+}
+___x_cmd_agg_activate
---
+++ src/media/agg/.x-cmd/deactivate.sh
@@ -0,0 +1,4 @@
+___x_cmd_agg_deactivate(){
+    x path rm "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
+}
+___x_cmd_agg_deactivate
