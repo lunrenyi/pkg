e0146db5a4daa95ddd55001325abb2d84ad717ea
jzy-w <tangzhiwen@x-cmd.com>
2023-03-24 19:28:02 +0800

    update

--- src/osman/7za/.x-cmd/install.sh
+++ src/osman/7za/.x-cmd/install.sh
@@ -1,9 +1,12 @@
-if ! [ -d "$___X_CMD_PKG_INSTALL_PATH/$name/$version" ]; then
-    mkdir "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
+if ! [ -d "$___X_CMD_PKG_INSTALL_PATH/${name}/${version}" ]; then
+   x mkdir "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
 fi
-cp -r $ball "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za"
+cp -r $ball "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za.7z"
 
 (
-cd "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
-p7zd x "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za"
+cd "$___X_CMD_PKG_INSTALL_PATH/$name/$version" || {
+    pkg:error "Failed to cd to $___X_CMD_PKG_INSTALL_PATH/$name/$version"
+    return 1
+}
+p7zd x "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za.7z"
 )
