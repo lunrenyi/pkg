ce236ccc52ed5a03b048b9fd39a2195b3d46981c
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-24 20:18:58 +0800

    ...

--- src/osman/7za/.x-cmd/install.sh
+++ src/osman/7za/.x-cmd/install.sh
@@ -1,12 +1,12 @@
-if ! [ -d "$___X_CMD_PKG_INSTALL_PATH/${name}/${version}" ]; then
-    x mkdirp "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
-fi
+xrc p7zip
+x mkdirp "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
 cp -r $ball "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za.7z"
 
 (
-cd "$___X_CMD_PKG_INSTALL_PATH/$name/$version" || {
-    pkg:error "Failed to cd to $___X_CMD_PKG_INSTALL_PATH/$name/$version"
-    return 1
-}
-p7zd x "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za.7z"
+    cd "$___X_CMD_PKG_INSTALL_PATH/$name/$version" || {
+        pkg:error "Failed to cd to $___X_CMD_PKG_INSTALL_PATH/$name/$version"
+        return 1
+    }
+    p7zd x "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za.7z"
+    chmod +X "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za.$os.$arch"
 )
