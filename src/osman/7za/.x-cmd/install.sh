243348b8419364a1917856bd987a2b4a88e90a7a
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-24 18:44:19 +0800

    ...

--- src/osman/7za/.x-cmd/install.sh
+++ src/osman/7za/.x-cmd/install.sh
@@ -1,1 +1,9 @@
-p7zd x $ball "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
+if ! [ -d "$___X_CMD_PKG_INSTALL_PATH/$name/$version" ]; then
+    mkdir "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
+fi
+cp -r $ball "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za"
+
+(
+cd "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
+p7zd x "$___X_CMD_PKG_INSTALL_PATH/$name/$version/7za"
+)
