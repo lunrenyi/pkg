ffd5c86e66f07437be6c18784a9a968487466b1b
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-27 11:03:34 +0800

    ...

--- src/lang/lua/.x-cmd/deactivate.sh
+++ src/lang/lua/.x-cmd/deactivate.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
 ___x_cmd_lua_deactivate(){
-    x path rm "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin"
+    x path rm "$___X_CMD_PKG_INSTALL_PATH/$name/$version"
 }
 
 ___x_cmd_lua_deactivate
