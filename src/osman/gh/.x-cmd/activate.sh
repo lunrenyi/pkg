7adb6c7b1da9cf416c9add05ea05df62cd8781d8
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-27 20:06:56 +0800

    ...

--- src/osman/gh/.x-cmd/activate.sh
+++ src/osman/gh/.x-cmd/activate.sh
@@ -1,9 +1,9 @@
 # shellcheck shell=sh disable=SC2154,SC2115
 ___x_cmd_gh_activate(){
     if ! [ -f "$___X_CMD_PKG_BIN_PATH/$name" ]; then
-        command ln -s "$___X_CMD_PKG_INSTALL_PATH/$name/$version/$name.$os.$arch"  "$___X_CMD_PKG_BIN_PATH/$name" 2>/dev/null
+        command ln -s "$___X_CMD_PKG_INSTALL_PATH/$name/$version/bin/$name"  "$___X_CMD_PKG_BIN_PATH/$name" 2>/dev/null
     fi
-    ___x_cmd_path_unshift "$___X_CMD_PKG_BIN_PATH"
-    x path uniq
+
+
 }
 ___x_cmd_gh_activate
--- src/osman/terraform/.x-cmd/activate.sh
+++ src/osman/terraform/.x-cmd/activate.sh
@@ -3,7 +3,7 @@ ___x_cmd_terraform_activate(){
     if ! [ -f "$___X_CMD_PKG_BIN_PATH/$name" ]; then
         command ln -s "$___X_CMD_PKG_INSTALL_PATH/$name/$version/$name.$os.$arch"  "$___X_CMD_PKG_BIN_PATH/$name" 2>/dev/null
     fi
-    ___x_cmd_path_unshift "$___X_CMD_PKG_BIN_PATH"
-    x path uniq
+
+
 }
 ___x_cmd_terraform_activate
