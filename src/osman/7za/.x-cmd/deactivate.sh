b977845c7c0f402348d7540bc0439fed9e6efcd0
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-27 16:08:13 +0800

    ...

---
+++ src/osman/7za/.x-cmd/deactivate.sh
@@ -0,0 +1,4 @@
+___x_cmd_7za_deactivate(){
+    rm -rf "$___X_CMD_PKG_BIN_PATH/$name"
+}
+___x_cmd_7za_deactivate
---
+++ src/osman/mosquitto/.x-cmd/activate.sh
@@ -0,0 +1,8 @@
+___x_cmd_mosquitto_activate(){
+    if ! [ -f "$___X_CMD_PKG_BIN_PATH/$name" ]; then
+        command ln -s "$___X_CMD_PKG_INSTALL_PATH/$name/$version/$name.$os.$arch"  "$___X_CMD_PKG_BIN_PATH/$name" 2>/dev/null
+    fi
+    ___x_cmd_path_unshift "$___X_CMD_PKG_BIN_PATH"
+    x path uniq
+}
+___x_cmd_mosquitto_activate
