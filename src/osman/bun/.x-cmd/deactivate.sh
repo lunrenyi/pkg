64f80636896a18d5fbb6cf41a221da4645d4f01b
zhengyuhang <zhengyuhang@x-cmd.com>
2023-03-27 16:35:50 +0800

    ...

--- src/osman/bun/.x-cmd/deactivate.sh
+++ src/osman/bun/.x-cmd/deactivate.sh
@@ -1,4 +1,4 @@
-# shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
+# shellcheck shell=sh disable=SC2154,SC2115
 ___x_cmd_bun_deactivate(){
     rm -rf "$___X_CMD_PKG_BIN_PATH/$name"
 }
--- src/osman/exa/.x-cmd/activate.sh
+++ src/osman/exa/.x-cmd/activate.sh
@@ -1,4 +1,4 @@
-# shellcheck shell=sh disable=SC2039,SC1090,SC3043,SC2263
+# shellcheck shell=sh disable=SC2154,SC2115
 ___x_cmd_exa_activate(){
     if ! [ -f "$___X_CMD_PKG_BIN_PATH/$name" ]; then
         command ln -s "$___X_CMD_PKG_INSTALL_PATH/$name/$version/$name.$os.$arch"  "$___X_CMD_PKG_BIN_PATH/$name" 2>/dev/null
