332f51b793e3937c8055ed6fdc48f3847e6c3a91
zhengyuhang <zhengyuhang@x-cmd.com>
2023-04-04 11:56:16 +0800

    ...

---
+++ src/sdkman/bpipe/version.sh
@@ -0,0 +1,15 @@
+get_bpipe_version(){
+    curl https://api.github.com/repos/ssadedin/bpipe/releases 2>/dev/null | x jo env .\* .tag_name -- 'printf "%s\n" "
+$tag_name:
+  linux/x64:
+    sha:
+  darwin/amd64:
+    sha:
+  darwin/x86:
+    sha:
+  win/x64:
+    sha:
+    "'
+}
+
+get_bpipe_version | x yq -o json e -P
---
+++ src/sdkman/btrace/version.sh
@@ -0,0 +1,15 @@
+get_btrace_version(){
+    curl https://api.github.com/repos/btraceio/btrace/releases 2>/dev/null | x jo env .\* .tag_name -- 'printf "%s\n" "
+$tag_name:
+  linux/x64:
+    sha:
+  darwin/amd64:
+    sha:
+  darwin/x86:
+    sha:
+  win/x64:
+    sha:
+    "'
+}
+
+get_btrace_version | x yq -o json e -P
