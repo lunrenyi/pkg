3285b711ab57fff31838d555b0b4c869025e7cd2
zhengyuhang <zhengyuhang@x-cmd.com>
2023-04-05 23:09:49 +0800

    ...

---
+++ src/sdkman/concurnas/version.sh
@@ -0,0 +1,15 @@
+get_concurnas_version(){
+    curl https://api.github.com/repos/Concurnas/Concurnas/releases 2>/dev/null | x jo env .\* .tag_name -- 'printf "%s\n" "
+$tag_name:
+  linux/x64:
+    sha:
+  darwin/amd64:
+    sha:
+  darwin/x86:
+    sha:
+  win/x64:
+    sha:
+    "'
+}
+
+get_concurnas_version | x yq -o json e -P
